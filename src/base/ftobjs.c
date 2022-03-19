f014853f842a9a9ae4c4e310ee9bf0cd796dc0f5
Werner Lemberg <wl@gnu.org>
2022-03-19 06:40:17 +0100

    DO NOT MERGE - Cherry-pick two upstream changes
    This cherry picks following two changes:
    
    0c2bdb01a2e1d24a3e592377a6d0822856e10df2
    22a0cccb4d9d002f33c1ba7a4b36812c7d4f46b5
    
    Bug: 271680254
    Test: N/A
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4ffa271ab538f57b65a65d434a2df9d3f8cd2f4a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b0f8930701bf19229075cc930ad15813ff5fb07b)
    Merged-In: I42469df8e8b07221d64e3f8574c4f30110dbda7e
    Change-Id: I42469df8e8b07221d64e3f8574c4f30110dbda7e
    
    Change-Id: Ia584ab1f2633a84e5896c09113513c582ce95dad

--- src/base/ftobjs.c
+++ src/base/ftobjs.c
@@ -2184,6 +2184,16 @@
     FT_Module*   cur;
     FT_Module*   limit;
 
+
+    /* only use lower 31 bits together with sign bit */
+    if ( face_index > 0 )
+      face_index &= 0x7FFFFFFFL;
+    else
+    {
+      face_index &= 0x7FFFFFFFL;
+      face_index  = -face_index;
+    }
+
 #ifndef FT_CONFIG_OPTION_MAC_FONTS
     FT_UNUSED( test_mac_fonts );
 #endif
@@ -3063,6 +3073,9 @@
 
     if ( !face )
       return FT_THROW( Invalid_Face_Handle );
+
+    if ( !face->size )
+      return FT_THROW( Invalid_Size_Handle );
 
     if ( !req || req->width < 0 || req->height < 0 ||
          req->type >= FT_SIZE_REQUEST_TYPE_MAX )
